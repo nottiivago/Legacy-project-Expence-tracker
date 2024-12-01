1c1435ab28e8e48be9fa8b560e30440e2c6522a7
DennisMantz <d.mantzavinatos@gmail.com>
2024-12-01 13:24:08 +0200

    last changes bg

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -49,8 +49,13 @@ function Homepage() {
 //   };
 
   return (
-    <div className="App h-full px-3">
-      <div className="mt-5 mx-auto h-full border-2 border-black rounded-md max-w-[350px] sm:max-w-[500px]">
+    <div className="min-h-screen px-3 bg-cover bg-center bg-no-repeat pt-5 " 
+    style={{
+      backgroundImage: "url('/assets/Back.webp')",
+    }}
+  >
+      
+      <div className="mt-5 mx-auto  border-2 border-black rounded-md max-w-[350px] sm:max-w-[500px]">
         <h1 className=" pl-2 flex justify-start text-xl font-bold  ">
           <span className=" border-2 border-green-400">Cash Over</span>flow
         </h1>
@@ -103,6 +108,13 @@ function Homepage() {
         </h4>
       </div>
 
+      <div className="px-3 mt-5 h-[80px] w-[80px] mx-auto bg-cover border-black border-2 rounded-full" 
+    style={{
+      backgroundImage: "url('/assets/Logo1.webp')",
+    }}
+  >
+
+      </div>
       {/* <div className="mt-3 mx-auto flex justify-around max-w-[350px] sm:max-w-[500px]">
         <div className="rounded-full w-20 h-20 sm:w-28 sm:h-28 border-black border-2 flex justify-center items-center">
           <button className="">Option1</button>
